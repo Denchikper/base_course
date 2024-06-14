4a2791966a928e688b92e2f874c3f9dcc73d00b2
Denchi <69343484+Denchikper@users.noreply.github.com>
2024-06-14 13:14:49 +0000

    1

---
+++ lec_1_data_type.py
@@ -0,0 +1,19 @@
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type("Hello"))
+
+a = "Molodec"
+print(a, type(a))
+
+a = ["123", 1, '2323234', 23, 5]
+print(a, type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(print(3))
