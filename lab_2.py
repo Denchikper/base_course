7983cf8376cfd7af43c9b90af892878dfcaf2487
Denchi <69343484+Denchikper@users.noreply.github.com>
2024-06-25 13:58:30 +0000

    25.06

--- lab_2.py
+++ lab_2.py
@@ -1,8 +1,7 @@
 from numpy import *
-from math import sqrt
 from lab_1 import g
 h = 100
 a= 45
 b = 35
-v = sqrt((g * tan**2(b)) / 2 * cos**2(a) * (1 - tan(b) * tan(a)))
+v = sqrt((g * tan(b)**2) / 2 * cos(a)**2 * (1 - tan(b) * tan(a)))
 print()
